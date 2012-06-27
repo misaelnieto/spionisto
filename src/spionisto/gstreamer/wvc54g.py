e78b05bf3198fd14bd4ff8968724fa37c811f648
tzicatl <tzicatl@gmail.com>
2012-06-26 20:49:06 -0500

    Fix pipeline for wvc54g

--- src/spionisto/gstreamer/wvc54g.py
+++ src/spionisto/gstreamer/wvc54g.py
@@ -6,8 +6,8 @@ _P = [
     'souphttpsrc location=http://%s/img/video.asf',
     'asfdemux',
     'ffdec_mpeg4',
-    'video/x-raw-rgb, framerate=15/1, width=640, height=480',
-    'clockoverlay text="%s" valign=bottom shaded-background=true font-desc="Courier bold 24"'
+    # 'video/x-raw-rgb, framerate=15/1, width=640, height=480',
+    'clockoverlay text="%s" valign=bottom shaded-background=true font-desc="Courier bold 24"',
     'jpegenc',
     'multipartmux boundary=spionisto',
     'tcpclientsink port=%s',
